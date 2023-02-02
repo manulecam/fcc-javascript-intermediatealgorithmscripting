9b4da4fa1b6bd4fd032665d1a3dbe31cf25c906f
manulecam <lecam.manuel@gmail.com>
2023-02-02 17:07:00 -0300

    update commit

---
+++ suma-todos-los-nmeros-primos.js
@@ -0,0 +1,17 @@
+function sumPrimes(num) {
+  function isPrime(num) {
+    const sqrt = Math.sqrt(num);
+    for (let i = 2; i <= sqrt; i++) {
+      if (num % i === 0)
+        return false;
+    }
+    return true;
+  }
+
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i))
+      sum += i;
+  }
+  return sum;
+}
